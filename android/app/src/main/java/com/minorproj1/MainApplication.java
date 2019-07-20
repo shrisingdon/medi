7a1abf70b1a63ed2b2d6a318e00d256ed6930a06
aryaminus <sunim.54@gmail.com>
2019-07-20 17:58:42 +0545

    Fix the build as wrong version was implemented

--- android/app/src/main/java/com/minorproj1/MainApplication.java
+++ android/app/src/main/java/com/minorproj1/MainApplication.java
@@ -6,6 +6,7 @@ import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;                       import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactApplication;
+import pro.equinoxstudio.react_native_alarm_clock.RNAlarmClockPackage;
 //import io.invertase.firebase.RNFirebaseAdMobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -28,15 +29,16 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList (
+      return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNAlarmClockPackage(),
           //new RNFirebaseAdMobPackage(),
           new VectorIconsPackage(),
           new MainReactPackage(),
           new RNFirebasePackage(),
           new RNFirebaseMessagingPackage(),
           new RNFirebaseNotificationsPackage(),
-          new RNGestureHandlerPackage(),
+          new RNGestureHandlerPackage()
       );
     }
 
@@ -46,47 +48,10 @@ public class MainApplication extends Application implements ReactApplication {
     }
   };
 
-  /**
- * Called if InstanceID token is updated. This may occur if the security of
- * the previous token had been compromised. Note that this is called when the InstanceID token
- * is initially generated so this is where you would retrieve the token.
- */
-@Override
-public void onNewToken(String token) {
-    Log.d(TAG, "Refreshed token: " + token);
-
-    // If you want to send messages to this application instance or
-    // manage this apps subscriptions on the server side, send the
-    // Instance ID token to your app server.
-    sendRegistrationToServer(token);
-
   @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
   }
-
-  @Override
-  public void onClick (View v) {
-    // Get token
-    // [START retrieve_current_token]
-    FirebaseInstanceId.getInstance().getInstanceId()
-      .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
-        @Override
-          public void onComplete(@NonNull Task<InstanceIdResult> task) {
-            if (!task.isSuccessful()) {
-              Log.w(TAG, "getInstanceId failed", task.getException());
-              return;
-            }
-
-          // Get new Instance ID token
-            String token = task.getResult().getToken();
-
-            // Log and toast
-            String msg = getString(R.string.msg_token_fmt, token);
-            Log.d(TAG, msg);
-            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-      }
-    });
 
   @Override
   public void onCreate() {
@@ -94,3 +59,50 @@ public void onNewToken(String token) {
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
+
+// public class MainApplication extends Application implements ReactApplication {
+
+  /**
+ * Called if InstanceID token is updated. This may occur if the security of
+ * the previous token had been compromised. Note that this is called when the InstanceID token
+ * is initially generated so this is where you would retrieve the token.
+ */
+// @Override
+// public void onNewToken(String token) {
+//     Log.d(TAG, "Refreshed token: " + token);
+
+//     // If you want to send messages to this application instance or
+//     // manage this apps subscriptions on the server side, send the
+//     // Instance ID token to your app server.
+//     sendRegistrationToServer(token);
+
+//   @Override
+//   public ReactNativeHost getReactNativeHost() {
+//     return mReactNativeHost;
+//   }
+
+//   @Override
+//   public void onClick (View v) {
+//     // Get token
+//     // [START retrieve_current_token]
+//     FirebaseInstanceId.getInstance().getInstanceId()
+//       .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+//         @Override
+//           public void onComplete(@NonNull Task<InstanceIdResult> task) {
+//             if (!task.isSuccessful()) {
+//               Log.w(TAG, "getInstanceId failed", task.getException());
+//               return;
+//             }
+
+//           // Get new Instance ID token
+//             String token = task.getResult().getToken();
+
+//             // Log and toast
+//             String msg = getString(R.string.msg_token_fmt, token);
+//             Log.d(TAG, msg);
+//             Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//       }
+//     });
+
+// }
+// }
